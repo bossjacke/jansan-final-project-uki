8ee4cb6cc215ca34a71881a5201a85baa625fc9a
jansan <bossjansan1234@gmail.com>
2025-10-30 06:47:11 +0530

    a

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true, unique: true, lowercase: true },
+        phone: { type: String, required: true },
+        password: { type: String, required: true },
+        role: { type: String, enum: ["admin", "customer","buier"], default: "customer" },
+        locationId: {
+            type: String,
+            required: function () {
+                return this.role === "customer";
+            },
+        },
+    },
+    { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
