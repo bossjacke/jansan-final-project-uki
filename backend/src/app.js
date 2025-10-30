93348fe3e595d29fe4e124438bc6c972228bdb1d
jansan <bossjansan1234@gmail.com>
2025-10-30 15:12:19 +0530

    router

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/user.routes.js";
+import router from "./routes/product.routes.js";
 
 dotenv.config();
 const app = express();
@@ -21,6 +22,9 @@ app.use(express.json());
 
 // Routes
 app.use("/api/users", userRoutes);
+
+app.use("api/products", router);
+
 
 // Database connection
 connectDB();
