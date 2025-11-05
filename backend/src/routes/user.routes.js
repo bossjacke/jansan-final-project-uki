99397977bfdb3498cfdaf9ed3ba848c7bf81b778
jansan <bossjansan1234@gmail.com>
2025-11-05 13:23:09 +0530

    getAllUsers

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/user.controller.js";
 //get user
-import { getUserProfile,updateUserProfile,deleteUser } from "../controllers/user.controller.js";
+import { getUserProfile,updateUserProfile,deleteUser,getAllUsers } from "../controllers/user.controller.js";
 import { authMiddleware } from "../middleware/auth.js";
 
 
@@ -20,6 +20,7 @@ router.post("/login", loginUser);
 // GET /api/users/profile
 router.get("/profile", authMiddleware, getUserProfile);
 router.put("/profile", authMiddleware,    updateUserProfile);
+router.get("/", authMiddleware, getAllUsers);
 router.delete("/:userId", authMiddleware, deleteUser   );
 
 
